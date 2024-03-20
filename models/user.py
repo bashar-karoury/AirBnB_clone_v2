8fa2478fdc051570ebdc6a6ce1d91c0e4fd8beb1
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-20 13:45:22 +0400

    Fix user

--- models/user.py
+++ models/user.py
@@ -10,8 +10,8 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     places = relationship("Place", cascade="all, delete", backref="user")
