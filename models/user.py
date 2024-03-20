89c1207d3dbdd22a61e909132e4b9bdc84033da0
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-21 01:06:38 +0400

    Separate definition of file storage and db storage classed

--- models/user.py
+++ models/user.py
@@ -5,14 +5,22 @@ from models.base_model import BaseModel, Base
 # from models.review import Review
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+import os
+storage_env = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+    if storage_env == 'db':    
+        __tablename__ = 'users'
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", cascade="all, delete", backref="user")
+        reviews = relationship("Review", cascade="all, delete", backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
