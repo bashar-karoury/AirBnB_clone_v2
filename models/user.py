406a131cba69504808738e558575f07b63f62d52
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-20 13:36:43 +0400

    Fix user

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
-from sqlalchemy import Column, Integer, String, ForeignKey
+# from models.place import Place
+# from models.review import Review
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 
