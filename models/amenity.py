cc52969507c3a0279014ae0885f7163f8976bb92
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-21 00:12:16 +0400

    Add setter and getter for amenities in Place

--- models/amenity.py
+++ models/amenity.py
@@ -5,8 +5,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+
+
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities  = relationship("Place", secondary=place_amenity, backref="amenities", viewonly=False)
-
+    place_amenities = relationship(
+        "Place",
+        secondary=place_amenity,
+        backref="amenities",
+        viewonly=False)
