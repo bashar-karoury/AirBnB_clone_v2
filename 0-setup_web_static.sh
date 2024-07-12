0968a5f2aa7d3fd7e2958707ff844384f8f3d18a
bashar-karoury <basharalkaroury@hotmail.com>
2024-07-12 14:32:00 +0400

    Fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,55 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# Shell Script to set up the server for the deployment of web_static
+package="nginx"
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+# Check if the nginx is installed
+if ! dpkg -l | grep -q "^ii  $package"; then
+    sudo apt-get update
+    sudo apt-get install -y $package
+fi
+
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+content="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
+
+echo "$content" > /data/web_static/releases/test/index.html
+ln -sfr /data/web_static/releases/test/ /data/web_static/current
+user="ubuntu"
+group="ubuntu"
+
+# Change the ownership of the directory
+sudo chown -hR $user:$group /data
+
+nginx_config="/etc/nginx/sites-available/default"
+web_static_dir="/data/web_static/current"
+
+# Update Nginx configuration
+sudo tee $nginx_config > /dev/null <<EOF
+server {
+    listen 80;
+    server_name static_web;
+    root /;
+    location / {
+        try_files \$uri \$uri/ =404;
+	add_header X-Served-By \$hostname;
+    }
+
+    location /hbnb_static {
+        alias $web_static_dir/;
+	try_files \$uri \$uri/ =404;
+	add_header X-Served-By \$hostname;
+    }
+}
+EOF
+
+# Test Nginx configuration
+sudo nginx -t > /dev/null 2>&1
+
+# Restart Nginx to apply changes
+sudo service nginx start > /dev/null 2>&1
