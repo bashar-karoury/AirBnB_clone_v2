ad1a326176b9a18a1218f92fa481944b82c9c4bb
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-02 08:34:28 +0300

    ِAdd

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" python script to run flask application that
+     list of all State objects present in DBStorage sorted by name (A->Z)
+"""
+from flask import Flask, render_template
+from models import storage, State
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+	states = storage.all(State)
+	return render_template('7-states_list.html', states_to_list=states)
+	#return "All good till now"
+
+@app.teardown_appcontext
+def close_session(exception=None):
+	"""close session after each request
+	"""
+	storage.close()
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
