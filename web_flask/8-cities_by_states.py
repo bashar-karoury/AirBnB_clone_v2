d1e49ce4f7f4998209ffed576f0181b84aee68dc
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-02 16:17:43 +0300

    Add task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,7 +7,7 @@ from models import storage, State, City
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("cities_by_states", strict_slashes=False)
 def states_list():
     states = storage.all(State)
     s_l = list(states.values())
